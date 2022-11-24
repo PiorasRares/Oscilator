cc165e83a4d7c66dfaee768112dac21984037246
PiorasRares <pioras.rares95@gmail.com>
2022-11-24 13:19:07 +0200

    Add project files.

---
+++ Oscilator/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Oscilator;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
